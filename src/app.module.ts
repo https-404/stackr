c16d6e4c49d2f300c47675aa9eea3d15e10b7e1b
Areesh Ali <areesh.192003@gmail.com>
2025-12-18 16:20:42 +0500

    feat: add DatabaseModule for PostgreSQL connection and update .env.example

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { IamModule } from './iam/iam.module';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
-  imports: [IamModule],
+  imports: [IamModule, DatabaseModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,8 @@ import { ValidationPipe } from '@nestjs/common';
 import 'dotenv/config';
 
 async function bootstrap() {
+  console.log('Database URL: ', process.env.DATABASE_URL);
+  console.log('THE APP IS AVAILABLE ON PORT:', process.env.PORT);
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   await app.listen(process.env.PORT ?? 3000);
